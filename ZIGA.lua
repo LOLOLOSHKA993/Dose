b8a2ab5daf95b16335b8c3a49925f616284fda2e
LOLOLOSHKA993 <nazarkatrmc@gmail.com>
2024-12-28 21:41:32 +0100

    ZIGA2.lua

--- ZIGA.lua
+++ ZIGA.lua
@@ -1,1 +1,15 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LOLOLOSHKA993/Ziga/refs/heads/main/Zigamed1.lua"))()
+-- Получаем персонажа игрока
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+
+-- Получаем правую руку персонажа
+local rightArm = character:WaitForChild("RightUpperArm")
+
+-- Получаем Motor6D, который управляет движением правой руки
+local motor = rightArm:FindFirstChild("RightShoulder")
+
+-- Проверяем, если Motor6D существует
+if motor then
+    -- Поднимаем руку на 50 градусов
+    motor.C0 = motor.C0 * CFrame.Angles(math.rad(50), 0, 0)
+end
